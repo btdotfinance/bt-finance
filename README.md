3c92798d133b9674dc19065ba39708224230c5f8
btdotfinance <btdotfinance@gmail.com>
2021-01-28 04:03:45 +0000

    update

---
+++ README.md
@@ -0,0 +1,77 @@
+# BT.Finance Contracts
+
+ 
+
+**BTToken:**[0x76c5449F4950f6338A393F53CdA8b53B0cd3Ca3a](https://etherscan.io/address/0x76c5449f4950f6338a393f53cda8b53b0cd3ca3a)  
+
+**DiffChecker:** [BT Token and UNI Token](https://www.diffchecker.com/FjZy74l0)
+
+**RewardsFactory:**[0x3A5BEE9a0B2B1d26A57bdcD78A3541e8B6142304](https://etherscan.io/address/0x3a5bee9a0b2b1d26a57bdcd78a3541e8b6142304)
+
+**DiffChecker:**[BT RewardsFactory and UNI RewardsFactory](https://www.diffchecker.com/K2ZnehSx)
+
+**Controller:**[0x03D2079c54967f463Fd6e89E76012F74EBC62615](https://etherscan.io/address/0x03d2079c54967f463fd6e89e76012f74ebc62615)
+
+**DiffChecker:**[BT Controller and YFI Controller](https://www.diffchecker.com/AiaVLTwk)
+
+**Autostake:**[0x12A50C5fa45d024DbC92f852669488DC3c9c4c17](https://etherscan.io/address/0x12a50c5fa45d024dbc92f852669488dc3c9c4c17)
+
+**DiffChecker:**[BT Autostake and FARM autostake](https://www.diffchecker.com/IxQOAiVL)
+
+**Timelock:** [0x8A93B2c8d6DDf0b2932abF2F062c95735b35243C](https://etherscan.io/address/0x8A93B2c8d6DDf0b2932abF2F062c95735b35243C)
+
+**DiffChecker:**[BT Timelock and Pickle Timelock](https://www.diffchecker.com/dvszqcHc)
+
+**Vote:**[0x85E129b851d07D942e743f9444D637f57d3E3767](https://etherscan.io/address/0x85E129b851d07D942e743f9444D637f57d3E3767)
+
+**DiffChecker:**[BT Vote and Pickle Vote](https://www.diffchecker.com/jGwllLHl)
+
+ 
+
+**strategyETHAlpha:**[0x17DBC753Cc51beBcd7dFb4CAAFc91129285dC975](https://etherscan.io/address/0x17DBC753Cc51beBcd7dFb4CAAFc91129285dC975)
+
+**strategyPickleWBTC:**[0xEa135c1c267124107F448f454Ad33f24FF521702](https://etherscan.io/address/0xEa135c1c267124107F448f454Ad33f24FF521702)
+
+**strategyPickleUSDC:**[0x15bA709f87DA04B6140892B3bAEc8F710cdF67d8](https://etherscan.io/address/0x15bA709f87DA04B6140892B3bAEc8F710cdF67d8)
+
+**strategyPickle3Crv:**[0x70b2FD1F90f8396cDB8e1e513b34b34337B7438D](https://etherscan.io/address/0x70b2FD1F90f8396cDB8e1e513b34b34337B7438D)
+
+**strategyPickleCRVRenBTC:**[0x941BA82720D4Cf9bb6412ffDB8e6d10a46B669BB](https://etherscan.io/address/0x941BA82720D4Cf9bb6412ffDB8e6d10a46B669BB)
+
+ 
+
+***Security is our first priority,Most of our code comes from YFI and Uniswap,audit still pending With PeckShield,the report will come out in January,We will set timelock and multi-sig DAO later.***
+
+***This project is in beta,use at your own risk.We will also have buy insurance from Nexus Mutual and Cover Protocol besides the security audit.***
+
+ 
+
+# BT Rewards Pool
+
+**Pool 1:**[0x5ae55a3249817B77cc88159dA912015c6a2c49Ed](https://etherscan.io/address/0x5ae55a3249817b77cc88159da912015c6a2c49ed)
+
+**Pool 2:**[0xC74d15D2e61414C0975B9DC31fA8921c9909D08D](https://etherscan.io/address/0xc74d15d2e61414c0975b9dc31fa8921c9909d08d)
+
+**ETH Vault Pool:**[0x617887F9410d8EbD6046637958663f13b68f570c](https://etherscan.io/address/0x617887f9410d8ebd6046637958663f13b68f570c)
+
+**WBTC Vault Pool:**[0x9398a5E5E4D84C6916AFcC8E13BE3965e0A73677](https://etherscan.io/address/0x9398a5e5e4d84c6916afcc8e13be3965e0a73677)
+
+**USDC Vault Pool:**[0xfde75c3bc7060Cd83353619C98b4ce8e051BD2a6](https://etherscan.io/address/0xfde75c3bc7060cd83353619c98b4ce8e051bd2a6)
+
+**3Crv Vault Pool:**[0x43c6C8C991832fdDE038F3A51Af7247e0Af3Cd40](https://etherscan.io/address/0x43c6c8c991832fdde038f3a51af7247e0af3cd40)
+
+**CRVRenWBTC Vault Pool:**[0xA2996f4E6909CF8A4b42A0568A1AE77fB84f7E89](https://etherscan.io/address/0xa2996f4e6909cf8a4b42a0568a1ae77fb84f7e89)
+
+ 
+
+# BT Team Wallet
+
+**Dev Fund**(**multi-sig**):[0xb31984fA824C5Fa29974afe534cd033979279737](https://etherscan.io/address/0xb31984fa824c5fa29974afe534cd033979279737)
+
+**Gov Fund**(**multi-sig**):[0x5dC224FA0aef9bAdA1201e05Bf978671F9c0Be6b](https://etherscan.io/address/0x5dc224fa0aef9bada1201e05bf978671f9c0be6b)
+
+**Treasury**(**multi-sig**):[0x80411aA06c3486DEa6eCefaA413834819E38370E](https://etherscan.io/address/0x80411aa06c3486dea6ecefaa413834819e38370e)
+
+**Operations Fund**:[0x9a9d38147a826864ed86bcda04c89d4a221f0af8](https://etherscan.io/address/0x9a9d38147a826864ed86bcda04c89d4a221f0af8)
+
+**After the project is stable, we will invite the authority and outstanding people from the community as keyholders on our Governance and Treasury multi-sig wallets.**
---
+++ contracts/strategies/V1.2/StrategySUSHISLPSUSHI.sol
@@ -0,0 +1,369 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-01-07
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.5.15;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function decimals() external view returns (uint);
+    function name() external view returns (string memory);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+
+        return c;
+    }
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
+
+library Address {
+    function isContract(address account) internal view returns (bool) {
+        bytes32 codehash;
+        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { codehash := extcodehash(account) }
+        return (codehash != 0x0 && codehash != accountHash);
+    }
+    function toPayable(address account) internal pure returns (address payable) {
+        return address(uint160(account));
+    }
+    function sendValue(address payable recipient, uint256 amount) internal {
+        require(address(this).balance >= amount, "Address: insufficient balance");
+
+        // solhint-disable-next-line avoid-call-value
+        (bool success, ) = recipient.call.value(amount)("");
+        require(success, "Address: unable to send value, recipient may have reverted");
+    }
+}
+
+library SafeERC20 {
+    using SafeMath for uint256;
+    using Address for address;
+
+    function safeTransfer(IERC20 token, address to, uint256 value) internal {
+        callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
+        callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+    function safeApprove(IERC20 token, address spender, uint256 value) internal {
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+    function callOptionalReturn(IERC20 token, bytes memory data) private {
+        require(address(token).isContract(), "SafeERC20: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = address(token).call(data);
+        require(success, "SafeERC20: low-level call failed");
+
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
+}
+
+interface Controller {
+    function vaults(address) external view returns (address);
+    function rewards() external view returns (address);
+}
+
+
+interface yERC20 {
+  function deposit(uint256 _pid, uint256 _amount) external;
+  function withdraw(uint256 _pid, uint256 _amount) external;
+  function pendingSushi(uint256 _pid, address _user) external view returns (uint);
+}
+
+interface UniswapRouter {
+  function swapExactTokensForTokens(
+      uint amountIn,
+      uint amountOutMin,
+      address[] calldata path,
+      address to,
+      uint deadline
+    ) external returns (uint[] memory amounts);
+    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB, uint liquidity);
+
+}
+
+interface ZAPPER {
+    function ZapIn(
+        address _FromTokenContractAddress,
+        address _pairAddress,
+        uint256 _amount,
+        uint256 _minPoolTokens,
+        address _allowanceTarget,
+        address _swapTarget,
+        bytes calldata swapData
+    ) external payable returns (uint256);
+}
+
+/*
+
+ A strategy must implement the following calls;
+
+ - deposit()
+ - withdraw(address) must exclude any tokens used in the yield - Controller role - withdraw should return to Controller
+ - withdraw(uint) - Controller | Vault role - withdraw should always return to vault
+ - withdrawAll() - Controller | Vault role - withdraw should always return to vault
+ - balanceOf()
+
+ Where possible, strategies must remain as immutable as possible, instead of updating variables, we update the contract by linking it in the controller
+
+*/
+contract StrategySUSHISLPSUSHI  {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+    uint public sushiIndex = 12;
+
+    address constant public unirouter = address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+    address constant public weth = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+	address constant public bt = address(0x76c5449F4950f6338A393F53CdA8b53B0cd3Ca3a);
+
+    address constant public want = address(0x795065dCc9f64b5614C407a6EFDC400DA6221FB0); //SUSHI_ETH SLP
+
+	address constant public SushiSwap = address(0xc2EdaD668740f1aA35E4D8f227fB8E17dcA888Cd);
+
+    address constant public SUSHI = address(0x6B3595068778DD592e39A122f4f5a5cF09C90fE2);
+
+    address constant public zapper = address(0xfF350eDc2242Ca4d7252A64746aec4A5487a852B);
+    address constant public pair = address(0x795065dCc9f64b5614C407a6EFDC400DA6221FB0); //SUSHI_ETH SLP
+    address constant public swap = address(0xDef1C0ded9bec7F1a1670819833240f027b25EfF);
+
+    address public governance;
+    address public controller;
+
+    uint256 public pledgeSLP = 0;
+    uint256 public redeliverynum = 100 * 1e18;
+
+    modifier onlyController {
+        require(msg.sender == controller, "!controller");
+        _;
+    }
+
+    constructor() public {
+        governance = tx.origin;
+        controller = 0x03D2079c54967f463Fd6e89E76012F74EBC62615;
+        doApprove();
+    }
+
+	function doApprove () internal{
+        IERC20(SUSHI).approve(unirouter, uint(-1));
+    }
+
+    function deposit() public {
+		uint256 _want = IERC20(want).balanceOf(address(this));
+		if (_want > 0)
+		{
+		    IERC20(want).safeApprove(SushiSwap, 0);
+            IERC20(want).safeApprove(SushiSwap, _want);
+		    yERC20(SushiSwap).deposit(sushiIndex,_want);
+            pledgeSLP = pledgeSLP.add(_want);
+		}
+    }
+
+
+    // Withdraw partial funds, normally used with a vault withdrawal
+    function withdraw(uint _amount) external onlyController
+	{
+		uint amount = _withdraw(_amount);
+		address _vault = Controller(controller).vaults(address(want));
+        require(_vault != address(0), "!vault");
+        IERC20(want).safeTransfer(_vault, amount);
+	}
+
+
+    function _withdraw(uint _amount) internal returns(uint) {
+		uint amount = IERC20(want).balanceOf(address(this));
+		if (amount < _amount) {
+            amount = _amount.sub(amount);
+            if(amount > pledgeSLP){
+                amount = pledgeSLP;
+            }
+			yERC20(SushiSwap).withdraw(sushiIndex,amount);
+            pledgeSLP = pledgeSLP.sub(amount);
+			amount = IERC20(want).balanceOf(address(this));
+            if (amount < _amount){
+                return amount;
+            }
+        }
+		return _amount;
+    }
+
+	function withdrawAll() external onlyController returns (uint balance){
+		uint amount = balanceOf();
+		balance = _withdraw(amount);
+
+		address _vault = Controller(controller).vaults(address(want));
+        require(_vault != address(0), "!vault");
+        IERC20(want).safeTransfer(_vault, balance);
+	}
+
+	function balanceOfwant() public view returns (uint256) {
+		return IERC20(want).balanceOf(address(this));
+	}
+
+
+    function balanceOf() public view returns (uint256) {
+        return balanceOfwant().add(pledgeSLP);
+    }
+
+    function getPending() public view returns (uint256) {
+        return yERC20(SushiSwap).pendingSushi(sushiIndex,address(this));
+    }
+
+	function getSUSHI() public view returns(uint256)
+	{
+		return IERC20(SUSHI).balanceOf(address(this));
+	}
+
+    function harvest() public
+    {
+        yERC20(SushiSwap).deposit(sushiIndex,0);    //get reward
+        redelivery();
+    }
+
+    function redelivery() internal {
+        uint256 reward = IERC20(SUSHI).balanceOf(address(this));
+        if (reward > redeliverynum) {
+            uint256 _2weth = reward.mul(80).div(100); //80%
+		    uint256 _2bt = reward.sub(_2weth);  //20%
+            _swapUniswap(SUSHI,weth, _2weth);
+			_redelivery();
+		    _swapUniswap(SUSHI,bt,_2bt);
+            IERC20(bt).safeTransfer(Controller(controller).rewards(), IERC20(bt).balanceOf(address(this)));
+		}
+        deposit();
+    }
+
+    function _redelivery() internal
+    {
+         uint256 _weth = IERC20(weth).balanceOf(address(this));
+        if (_weth > 0) {
+            IERC20(weth).safeApprove(zapper,0);
+		    IERC20(weth).safeApprove(zapper, _weth);
+            ZAPPER(zapper).ZapIn(
+                weth,
+                pair,
+                _weth,
+                0,
+                swap,
+                swap,
+                new bytes(0));
+        }
+    }
+
+     function _swapUniswap(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) internal {
+        require(_to != address(0));
+
+        // Swap with uniswap
+        IERC20(_from).safeApprove(unirouter, 0);
+        IERC20(_from).safeApprove(unirouter, _amount);
+
+        address[] memory path;
+
+        if (_from == weth || _to == weth) {
+            path = new address[](2);
+            path[0] = _from;
+            path[1] = _to;
+        } else {
+            path = new address[](3);
+            path[0] = _from;
+            path[1] = weth;
+            path[2] = _to;
+        }
+
+        UniswapRouter(unirouter).swapExactTokensForTokens(
+            _amount,
+            0,
+            path,
+            address(this),
+            now.add(1800)
+        );
+    }
+
+    function setredeliverynum(uint256 value) public
+    {
+        require(msg.sender == governance, "!governance");
+        redeliverynum = value;
+    }
+
+    function setGovernance(address _governance) public {
+        require(msg.sender == governance, "!governance");
+        governance = _governance;
+    }
+
+    function setController(address _controller) external {
+        require(msg.sender == governance, "!governance");
+        controller = _controller;
+    }
+}
